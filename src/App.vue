ecc693879a78ef6ee5561dc2d07d0343152b1447
Vitalii Posypenko <posypenkov@yandex.ru>
2023-10-25 23:27:45 +0700

    app initial

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+    import Vmain from '@/views/v-main.vue';
+</script>
+
+<template>
+    <Vmain />
+</template>
---
+++ src/views/v-main.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="main">
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" /> Logo
+        </a>
+        <button class="main__btn" type="button">Оставит заявку</button>
+    </main>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'main component',
+        setup() {}
+    }
+</script>
+
+<style lang="scss">
+    .main {
+        display: flex;
+        justify-content: space-between;
+        &__btn {
+            font-size: 14px;
+            color: #3740fa;
+        }
+    }
+</style>
